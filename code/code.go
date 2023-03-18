a895c164aaf772702d5b26ddad0efd39429c5740
ikiris <4852950+ikiris@users.noreply.github.com>
2023-03-18 17:30:24 -0500

    add pop support

--- code/code.go
+++ code/code.go
@@ -13,16 +13,18 @@ type Opcode byte
 const (
 	OpConstant Opcode = iota
 	OpAdd
+	OpPop
 )
-
-type Definition struct {
-	Name          string
-	OperandWidths []int
-}
 
 var definitions = map[Opcode]*Definition{
 	OpConstant: {"OpConstant", []int{2}},
 	OpAdd:      {"OpAdd", []int{}},
+	OpPop:      {"OpPop", []int{}},
+}
+
+type Definition struct {
+	Name          string
+	OperandWidths []int
 }
 
 func Lookup(op byte) (*Definition, error) {
