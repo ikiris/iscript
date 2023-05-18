a12f2500c8d2db94bb42643fa21df19508ebba68
ikiris <4852950+ikiris@users.noreply.github.com>
2023-05-18 18:20:16 -0500

    full conditionals w/ null

--- vm/vm.go
+++ vm/vm.go
@@ -108,6 +108,8 @@ func isTruthy(obj object.Object) bool {
 	switch obj := obj.(type) {
 	case *object.Boolean:
 		return obj.Value
+	case *object.Null:
+		return false
 	default:
 		return true
 	}
@@ -128,6 +130,8 @@ func (vm *VM) executeBangOperator() error {
 	case True:
 		return vm.push(False)
 	case False:
+		return vm.push(True)
+	case Null:
 		return vm.push(True)
 	default:
 		return vm.push(False)
