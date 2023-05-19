bfc89f60007e1a580ec45c3c6b7a522145d2d6e2
ikiris <4852950+ikiris@users.noreply.github.com>
2023-05-18 23:06:08 -0500

    hash literals part 1

--- code/code.go
+++ code/code.go
@@ -30,6 +30,7 @@ const (
 	OpGetGlobal
 	OpSetGlobal
 	OpArray
+	OpHash
 )
 
 var definitions = map[Opcode]*Definition{
@@ -52,6 +53,7 @@ var definitions = map[Opcode]*Definition{
 	OpGetGlobal:   {"OpGetGlobal", []int{2}},
 	OpSetGlobal:   {"OpSetGlobal", []int{2}},
 	OpArray:       {"OpArray", []int{2}},
+	OpHash:        {"OpHash", []int{2}},
 }
 
 type Definition struct {
